2735a7392925badc7c6c50529efc37d0c491a752
Jun su Jeon <jjc224@naver.com>
2023-03-18 01:41:37 +0900

    added Menu.java

---
+++ src/menu/Menu.java
@@ -0,0 +1,14 @@
+package menu;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1.add groceries");
+		System.out.println("2.edit groceries");
+		System.out.println("3.view groceries");
+		System.out.println("4.expenditure details");
+		System.out.println("5.exit");
+	}
+
+}
